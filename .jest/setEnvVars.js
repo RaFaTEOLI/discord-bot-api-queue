51d9d220b9c2786e9858bef5652e3b2a963e6bd4
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-09 09:54:36 -0300

    chore: add jest setup and axios package

---
+++ .jest/setEnvVars.js
@@ -0,0 +1,8 @@
+process.env.AMQP_USERNAME = 'discord-bot-api-queue';
+process.env.AMQP_PASSWORD = 'password';
+process.env.AMQP_PORT = 5672;
+process.env.AMQP_ADRESS = 'localhost';
+process.env.BOT_URL = 'discord-bot-player.com';
+process.env.BOT_API_URL = 'http://localhost:5050/api';
+process.env.BOT_API_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjUzMjhlZTU2ZDhlNzJiZWIyZDI0MiIsImlhdCI6MTY2NzU3NjQ2Mn0.CNrcITg4PqpFfC_eQrmh0umVvBDVgdtRHpdqC30-KEc';
