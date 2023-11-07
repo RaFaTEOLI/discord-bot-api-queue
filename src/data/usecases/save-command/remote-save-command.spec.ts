41a17f79882cdaab520a8d3614f558e59c2c6e06
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-11-07 10:24:09 -0300

    test: ensure RemoteSaveCommand will return void if HttpClient returns 201

--- src/data/usecases/save-command/remote-save-command.spec.ts
+++ src/data/usecases/save-command/remote-save-command.spec.ts
@@ -62,11 +62,11 @@ describe('RemoteSaveCommand', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 
-  test('should return void if HttpClient returns 200', async () => {
+  test('should return void if HttpClient returns 201', async () => {
     const { sut, httpClientSpy } = makeSut();
     const httpResult = mockSaveCommandParams();
     httpClientSpy.response = {
-      statusCode: HttpStatusCode.success,
+      statusCode: HttpStatusCode.created,
       body: httpResult
     };
     const response = await sut.save(mockSaveCommandParams());
