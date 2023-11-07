22492bb51fc1199d36f3c991a61850484f0404c7
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-11-07 10:27:13 -0300

    feat: ensure AmqpQueue will have acknowledge functions

--- src/domain/models/amqp-queue.ts
+++ src/domain/models/amqp-queue.ts
@@ -1,6 +1,18 @@
-import { type SaveCommand, type SaveMusic, type SaveQueue } from '@/domain/usecases';
+import {
+  type UpdateCommandStatus,
+  type SaveCommand,
+  type SaveMusic,
+  type SaveQueue,
+  type CommandStatus
+} from '@/domain/usecases';
 
 export type AmqpQueue = {
   action: 'music' | 'queue' | 'command';
   factory: SaveQueue | SaveMusic | SaveCommand;
+  ack?: {
+    function: UpdateCommandStatus;
+    functionName: 'update';
+    successPayload: CommandStatus;
+    failPayload: CommandStatus;
+  };
 };
--- src/main/server.ts
+++ src/main/server.ts
@@ -4,14 +4,25 @@ import 'dotenv/config';
 import {
   makeRemoteSaveMusicFactory,
   makeRemoteSaveQueueFactory,
-  makeRemoteSaveCommandFactory
+  makeRemoteSaveCommandFactory,
+  makeRemoteUpdateCommandStatusFactory
 } from '@/main/factories/usecases';
 import { type AmqpQueue } from '@/domain/models';
+import { CommandStatus } from '@/domain/usecases';
 
 const queues: AmqpQueue[] = [
   { action: 'music', factory: makeRemoteSaveMusicFactory() },
   { action: 'queue', factory: makeRemoteSaveQueueFactory() },
-  { action: 'command', factory: makeRemoteSaveCommandFactory() }
+  {
+    action: 'command',
+    factory: makeRemoteSaveCommandFactory(),
+    ack: {
+      functionName: 'update',
+      function: makeRemoteUpdateCommandStatusFactory(),
+      successPayload: CommandStatus.RECEIVED,
+      failPayload: CommandStatus.FAILED
+    }
+  }
 ];
 
 void (async () => {
@@ -34,13 +45,35 @@ void (async () => {
         queue.action,
         async message => {
           console.log(`📥 [${queue.action}] Received '%s'`, message.content.toString());
+          let id = null;
           try {
             const payload = JSON.parse(message.content.toString());
+
+            if (queue.ack) {
+              id = payload.id;
+              delete payload.id;
+            }
+
             await queue.factory.save(payload);
+
             channel.ack(message);
             console.log(`☑️ [${queue.action}] Acknowledged '%s'`, message.content.toString());
+
+            if (queue.ack) {
+              try {
+                await queue.ack.function[queue.ack.functionName](id, queue.ack.successPayload);
+                console.log(`☑️ [${queue.action}] Acknowledged Function '%s'`, queue.ack.functionName);
+              } catch (err) {
+                console.error('❌ Error while trying to call acknowledge function:', err.message);
+              }
+            }
           } catch (err) {
             console.error('❌ Error while trying to call factory:', err.message);
+
+            if (queue.ack) {
+              channel.ack(message);
+              await queue.ack.function[queue.ack.functionName](id, queue.ack.failPayload);
+            }
           }
         },
         { noAck: false }
