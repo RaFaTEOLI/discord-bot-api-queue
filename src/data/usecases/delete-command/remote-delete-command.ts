2f595772469d5f8e40f0e26b4b0a7346d92fe248
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-02-02 17:44:51 -0300

    feat: ensure RemoteDeleteCommand will throw AccessDeniedError if HttpClient returns 403

--- src/data/usecases/delete-command/remote-delete-command.ts
+++ src/data/usecases/delete-command/remote-delete-command.ts
@@ -1,13 +1,19 @@
-import { type HttpClient } from '@/data/protocols/http';
+import { HttpStatusCode, type HttpClient } from '@/data/protocols/http';
+import { AccessDeniedError } from '@/domain/errors';
 import { type DeleteCommand } from '@/domain/usecases';
 
 export class RemoteDeleteCommand implements DeleteCommand {
   constructor(private readonly url: string, private readonly httpClient: HttpClient<void>) {}
 
   async delete(id: string): Promise<void> {
-    await this.httpClient.request({
+    const httpResponse = await this.httpClient.request({
       url: `${this.url}/${id}`,
       method: 'delete'
     });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError();
+    }
   }
 }
